8c473ebd595026741c72b0dc96a15b1f569b70be
Cursyy <kurilets.o.v@gmail.com>
2024-10-05 00:55:43 +0100

    pages connected, css update

--- web_portal/urls.py
+++ web_portal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from .views import HomePageView, ManualListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('/manuals/', ManualListView.as_view(), name='manuals_list'),
+    # path('', HomePageView.as_view(), name='home'),
+    path('', ManualListView.as_view(), name='manuals_list'),
 ]
