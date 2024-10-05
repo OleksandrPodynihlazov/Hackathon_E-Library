ea6e42f16e54706093bd6659ae4d40d4b6e914a3
Cursyy <kurilets.o.v@gmail.com>
2024-10-05 01:29:32 +0100

    Images implemented, styles update

--- TUH_E_Library/settings.py
+++ TUH_E_Library/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static/'))]
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # Шлях до папки media
 MEDIA_URL = '/media/'  # URL, за яким будуть доступні файли
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Шлях до папки media
--- web_portal/models.py
+++ web_portal/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Manual(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='manual_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='static/media/', blank=True, null=True)
     manual_file = models.FileField(upload_to='manuals/', blank=True, null=True)
     
     def __str__(self):
