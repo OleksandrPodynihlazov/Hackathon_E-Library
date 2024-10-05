300ac475343bc998d2865544ab1771c8ee68a68d
ExclMark <info@3xcl.dev>
2024-10-05 01:27:44 +0100

    Account details

--- web_portal/urls.py
+++ web_portal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ManualListView
+from .views import ManualListView, AccountDetailView
 
 urlpatterns = [
     path('', ManualListView.as_view(), name='home'),
+    path('accounts/details', AccountDetailView.as_view(), name='account'),
 ]
--- web_portal/views.py
+++ web_portal/views.py
@@ -6,3 +6,7 @@ class ManualListView(LoginRequiredMixin, ListView):
     model = Manual
     template_name = 'manuals_page.html'
     context_object_name = 'manuals'
+
+class AccountDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'account.html'
+    context_object_name = 'account'
